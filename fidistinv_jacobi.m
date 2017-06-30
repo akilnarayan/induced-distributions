afd516a2c7d548fdd97638cf2230c6ab62c1425a
Akil Narayan <akil@sci.utah.edu>
2017-06-30 17:05:07 -0600

    New fast Jacobi method

--- fidistinv_jacobi.m
+++ fidistinv_jacobi.m
@@ -1,5 +1,5 @@
-function[x] = fidistinv_jacobi(u, n, alph, bet)
-% [x] = fidistinv_jacobi(u, n, alph, bet)
+function[x] = fast_jacobi(u, n, alph, bet);
+% [x] = fast_jacobi(u, n, alph, bet)
 %
 % A Fast Induced Distribution Inverse routine for Jacobi weights.
 %
@@ -8,38 +8,114 @@ function[x] = fidistinv_jacobi(u, n, alph, bet)
 % interpolant of the distribution function is computed and saved for future
 % runs.
 
-persistent options
-if isempty(options)
-  options = optimset(@fzero);
-  options.Display = 'off';
-  options.TolFun = 10*eps;
-  options.TolX = 10*eps;
-end
-
 data = load_fjacobi(n, alph, bet);
 
-if length(data) < n+1
+if length(data) < max(n(:))+1
 
-  data = fidistinv_jacobi_setup(n, alph, bet, data);
+  data = fast_jacobi_setup(n, alph, bet, data);
   save_fjacobi(data, alph, bet);
 
 end
 
 x = zeros(size(u));
-% Use data to generate an initial guess
-[~,j] = histc(u, data{n+1}.u);
-if any(j==0)
-  error('Input values u must be between 0 and 1');
+u = reshape(u, [numel(u) 1]);
+
+if numel(n) > 1
+  assert(numel(n) == numel(u));
+
+  % Bin points with same n values
+  [nsorted, inds] = sort(n);
+  indsep = find(diff(nsorted) > 0) + 1;
+else
+  nsorted = n;
+  inds = 1:numel(u);
+  indsep = [];
 end
 
-intervals = zeros([numel(u) 2]);
-intervals(:,1) = data{n+1}.breaks(j).';
+% Loop over n values
+for qn = 1:(length(indsep)+1)
+  if qn == 1
+    i1 = 1;
+    if length(indsep) == 0 % This means there's only 1 n value
+      i2 = numel(u);
+    else
+      i2 = indsep(qn)-1;
+    end
+  elseif qn == (length(indsep)+1)
+    i1 = indsep(qn-1);
+    i2 = numel(u);
+  else
+    i1 = indsep(qn-1);
+    i2 = indsep(qn)-1;
+  end
+  nn = nsorted(i1);
+  curr_ninds = inds(i1:i2);
+  uu = u(curr_ninds);
 
-flags = (j==numel(data{n+1}.u));
-intervals(~flags,2) = data{n+1}.breaks(j+1).';
-intervals(flags,2) = data{n+1}.breaks(end).';
+  % For this n value (=nn), compute Finv(uu)
+  M = size(data{nn+1},2);
+  N = size(data{nn+1},1) - 4; % Number of Chebyshev coeffs
 
-for q = 1:numel(u)
-  fun = @(xx) ppval(data{n+1}, xx) - u(q);
-  x(q) = fzero(fun, intervals(q,:), options);
+  itemp = [1, 2:2:M];
+  us = data{nn+1}(1,itemp).';
+  us = sort([us; 1/2*(us(1:end-1) + us(2:end))]);
+
+  % Bin uu values into subintervals
+  [~,j] = histc(uu, us);
+  if any(j==0)
+    error('Input values u must be between 0 and 1');
+  end
+
+  % Chebyshev setup + eval
+  [aa,bb] = jacobi_recurrence(N+1, -1/2, -1/2);
+  v = (uu - us(j))./(us(j+1) - us(j)) * 2 - 1;
+  V = poly_eval(aa, bb, v, N-1);
+
+  % Partition based on values of j
+  [jsorted, jinds] = sort(j);
+  jindsep = find(diff(jsorted) > 0) + 1;
+
+  xx = zeros(size(uu));
+
+  % Loop over subintervals
+  for jj = 1:(length(jindsep)+1)
+    if jj == 1
+      i1 = 1;
+      if length(jindsep) == 0
+        i2 = numel(uu);
+      else
+        i2 = jindsep(jj)-1;
+      end
+    elseif jj == (length(jindsep)+1)
+      i1 = jindsep(jj-1);
+      i2 = numel(uu);
+    else
+      i1 = jindsep(jj-1);
+      i2 = jindsep(jj)-1;
+    end
+
+    j = jsorted(i1);
+    currinds = jinds(i1:i2);
+
+    temp = V(currinds,:)*data{nn+1}(5:end,j);
+    if mod(j,2) == 0
+      sgn = -1;
+    else
+      sgn = +1;
+    end
+    temp = (temp + sgn)/2*data{nn+1}(4,j) ./ abs( uu(currinds) - data{nn+1}(1,j)).^data{nn+1}(3,j);
+    temp = temp + data{nn+1}(2,j);
+
+    xx(currinds) = temp;
+
+  end
+
+  x(curr_ninds) = xx;
+
 end
+
+% Make sure nothing is outside [-1,1]
+flags = abs(x) > 1;
+x(flags) = sign(x(flags));
+
+% For Half Freud: for large x the distribution looks like x^{2n+rho} exp(-x^alph) * x^(1-alph).
--- fidistinv_jacobi_setup.m
+++ fidistinv_jacobi_setup.m
@@ -1,10 +1,10 @@
-function[data] = fidistinv_jacobi(n, alph, bet, data)
-% data = fidistinv_jacobi(n, alph, bet, data)
+function[data] = fast_jacobi_setup(n, alph, bet, data)
+% data = fast_jacobi_setup(n, alph, bet, data)
 %
 % Setup computations for a Fast Induced Distribution Inverse routine for Jacobi
 % weights.
 %
-% Computes coefficients for a piecewise cubic interpolant of the degree-n
+% Computes coefficients for a piecewise Chebyshev interpolant of the degree-n
 % induced distribution for Jacobi weights. These coefficients are appended to
 % the data cell array, inside data{n+1}.
 %
@@ -17,13 +17,15 @@ if numel(ns) < 1
   return
 end
 
-% Number of points per sub interval in x
-Q = 20;
-xtemplate = linspace(0, 1, Q); xtemplate(1) = [];
-xtemplate = (sort(cos(pi*xtemplate)) + 1)/2;
+% Number of Chebyshev coefficients per subinterval
+N = 31;
 
-% Enrich with R points equidistantly in both x and u 
-R = 100;
+%%%% Generate Chebyshev grid + transform
+xgrid = flipud(cos(pi*(2*(0:(N-1)).'+1)/(2*N))); % equals gauss qaudrature nodes
+[aa,bb] = jacobi_recurrence(N+1, -1/2, -1/2);
+chebxform = poly_eval(aa, bb, xgrid, N-1);
+chebxform = (diag(1./sum(chebxform.^2, 2))*chebxform).';
+%%%%
 
 fprintf('One-time setup computations: Computing induced distribution data for...\n');
 
@@ -33,66 +35,76 @@ for q = 1:length(ns)
   nn = ns(q);
 
   fprintf('n = %d...\n', nn);
-  
-  % The data points we'll choose are as follows:
-  % R equally spaced data on u and x space
-  % Q data points on each interval bounded by zeros of p_nn (Q*(nn+1) + 1 points)
 
   [a,b] = jacobi_recurrence(nn+1, alph, bet);
-  x = gauss_quadrature(a, b, nn);
+  xg = gauss_quadrature(a,b,nn);
+  ug = idist_jacobi(xg, nn, alph, bet, 50); % Make it very accurate
 
-  x = [-1; x; 1];
+  us = [0;  ug;  1];
+  midpts = 1/2*( us(1:end-1) + us(2:end) );
+  temp = idistinv_jacobi(midpts, n, alph, bet);
 
-  % Create Q-1 points per interval
-  xn = diff(x) * xtemplate + repmat(x(1:end-1), [1 Q-1]);
-  xn = [reshape(xn.', [numel(xn) 1]); 1];
-  X = numel(xn);
+  [us, inds] = sort([0; ug; 1; midpts]);
+  xs = [-1; xg; 1; temp];
+  xs = xs(inds);
 
-  un = zeros(size(xn));
-  un = idist_jacobi(xn, nn, alph, bet);
+  dat = zeros([4+N 2*(nn+1)]);
+  % Data in each col: 
+  % ucenter, xcenter, exponent, scale (1-4)
+  % coeffs (5-end)
 
-  % Now R data points equally-spaced in u space
-  temp = linspace(0, 1, R + 2).';
-  temp([1 end]) = [];
+  for j=1:(nn+1)
+    i1 = 2*j-1;
+    i2 = 2*j;
 
-  un = [un; temp];
-  xn = [xn; idistinv_jacobi(temp, nn, alph, bet)];
+    exponents = [2/3 2/3];
 
-  % Now R data points equally-spaced in x space
-  temp = linspace(-1, 1, R+2).';
-  temp([1 end]) = [];
+    uleft = us(i1);
+    ucenter = us(i2);
+    uright = us(i2+1);
 
-  X = numel(xn);
-  xn = [xn; temp];
-  un = [un; zeros(size(temp))];
-  un((X+1):numel(xn)) = idist_jacobi(xn((X+1):numel(xn)), nn, alph, bet);
+    xleft = xs(i1);
+    xcenter = xs(i2);
+    xright = xs(i2+1);
 
-  % Sort
-  [xn, inds] = sort(xn);
-  un = un(inds);
-
-  % Some non-monotonic behavior happens very close to u=1. Remove these.
-  while true
-    inds = [false; diff(un) < 0];
-    if not(any(inds))
-      break
+    if j == 1
+      exponents(1) = bet/(bet+1);
     end
-    un(inds) = [];
-    xn(inds) = [];
+    if j==(nn+1)
+      exponents(2) = alph/(alph+1);
+    end
+
+    ucenters(1) = uleft;
+    ucenters(2) = uright;
+
+    xcenters(1) = xleft;
+    xcenters(2) = xright;
+
+    scales(1) = (xcenter - xcenters(1)) .* abs( ucenter - ucenters(1)).^exponents(1);
+
+    scales(2) = (xcenters(2) - xleft ) .* abs( uleft - ucenters(2)).^exponents(2);
+
+    % i1 coeffs
+    ugrid = (xgrid+1)/2*(ucenter-uleft) + uleft;
+    tgrid = idistinv_jacobi(ugrid, nn, alph, bet);
+    tgrid = (tgrid - xcenters(1)) .* abs(ugrid - ucenters(1)).^exponents(1);
+    tgrid = tgrid/scales(1)*2 - 1;
+    coeffs = chebxform*tgrid;
+
+    dat(:,i1) = [ucenters(1); xcenters(1); exponents(1); scales(1); coeffs];
+
+    % i2 coeffs
+    ugrid = (xgrid+1)/2*(uright-ucenter) + ucenter;
+    tgrid = idistinv_jacobi(ugrid, nn, alph, bet);
+    tgrid = (tgrid - xcenters(2)) .* abs(ugrid - ucenters(2)).^exponents(2);
+    tgrid = tgrid/scales(2)*2 +1;
+    coeffs = chebxform*tgrid;
+
+    dat(:,i2) = [ucenters(2); xcenters(2); exponents(2); scales(2); coeffs];
+
   end
 
-  % Remove identical x values
-  while true
-    inds = [false; diff(xn) <= 0];
-    if not(any(inds))
-      break
-    end
-    un(inds) = [];
-    xn(inds) = [];
-  end
-
-  data{nn+1} = pchip(xn, un);
-  data{nn+1}.u = un;
+  data{nn+1} = dat;
 
 end
 fprintf('Done\n');
